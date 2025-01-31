042cd4f0b1f339e447bfacc832626b6b32baba15
Chetra Chann <channchetra@gmail.com>
2025-01-31 15:16:21 +0700

    Change Plugin Name 💻

--- two-factor-telegram.php
+++ two-factor-telegram.php
@@ -1,15 +1,15 @@
 <?php
 
 /**
- * Plugin Name: WP 2FA with Telegram
- * Plugin URI: https://blog.dueclic.com/wordpress-autenticazione-due-fattori-telegram/
+ * Plugin Name: WP 2FA Telegram
+ * Plugin URI: https://sovichetra.com
  * Description: This plugin enables two factor authentication with Telegram by increasing your website security and sends an alert every time a wrong login occurs.
  * Version: 3.2
  * Requires at least: 6.0
  * Tested up to: 6.7
  * Requires PHP: 7.0
- * Author: dueclic
- * Author URI: https://www.dueclic.com
+ * Author: Chetra Chann, dueclic
+ * Author URI: https://sovichetra.com
  * Text Domain: two-factor-login-telegram
  * License: GPLv3
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
